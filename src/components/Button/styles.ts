e9a58a39978a19b932ef6bf0a7a67db501155656
tamiresataide <tamiresaataide@gmail.com>
2022-12-14 09:14:41 -0300

    implementação do botão desabilitado

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -14,4 +14,10 @@ export const ButtonContainer = styled.button`
         opacity: 0.6;
         cursor:pointer;
     }
+
+    :disabled {
+        opacity: 0.3;
+        cursor: not-allowed;
+        z-index: 0;
+    }
 `
